61ae0f5dcd0bdd893a37109a3633c6e9d61a8f28
lotfi0615 <lotfibenali403@gmail.com>
2024-10-12 21:22:44 +0200

    version final

---
+++ tp2/action.js
@@ -0,0 +1,35 @@
+window.onload = () => {
+
+    const paramsString = document.location.search;
+    const searchParams = new URLSearchParams(paramsString);
+
+
+    for (const param of searchParams) {
+
+        console.log(param);
+
+        const elementId = param[0];
+        const elementValue = param[1];
+        const element = document.getElementById(elementId);
+
+
+        if (element !== null) {
+            element.textContent = elementValue;
+        }
+
+
+        if (elementId === "address") {
+
+            const addressValue = elementValue;
+            element.href = `https://www.google.com/maps/search/?api=1&query=${addressValue}`;
+            element.textContent = addressValue;
+        }
+
+        else if (elementId === "email") {
+
+            const emailValue = elementValue;
+            element.href = `mailto:${emailValue}?subject=Hello!&body=What's up?`;
+            element.textContent = emailValue;
+        }
+    }
+};
