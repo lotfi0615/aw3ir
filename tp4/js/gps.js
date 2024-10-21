62f26ab3731424c49668a77d9d7864f2a721d7de
lotfi0615 <lotfibenali403@gmail.com>
2024-10-21 22:53:49 +0200

    version final

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -1,17 +1,35 @@
+let isGeolocationActive = false; // Variable pour vérifier si la géolocalisation est active
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
+        isGeolocationActive = true; // Indique que la géolocalisation a été utilisée
     } else {
-        document.querySelector("#map").innerHTML = "Geolocation is not supported by this browser.";
+        document.querySelector("#map").innerHTML = "La géolocalisation n'est pas supportée par ce navigateur.";
     }
 }
 
 function showPosition(position) {
-    var latlon = position.coords.latitude + "," + position.coords.longitude;
-    var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=14&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg`;
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const latlon = `${lat},${lon}`; // Format des coordonnées
 
-    document.querySelector("#map").innerHTML = `<img src='${img_url}'>`;
+    const img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=14&size=600x400&key=YOUR_API_KEY`;
+
+    // Mettre à jour le champ d'adresse seulement si la géolocalisation est active
+    if (isGeolocationActive) {
+        document.getElementById("adressePostale").value = latlon;
+    }
+
+    // Afficher la carte
+    document.querySelector("#map").innerHTML = `
+        <img src='${img_url}' style='max-width: 100%; height: auto;'>
+    `;
+
+    isGeolocationActive = false; // Réinitialiser l'état après utilisation
 }
+
+
 
 function showError(error) {
     switch (error.code) {
